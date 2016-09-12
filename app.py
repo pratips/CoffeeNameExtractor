909006716b6abd0b22dbcb6645931b3374cda0a3
ABZOOBA <pratipsamanta@gmail.com>
2016-09-12 16:04:30 +0530

    flask main

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+# import csvprocessor
+from coffee_name_recognizer import *
+app = Flask(__name__)
+
+
+@app.route("/")
+def main():
+    return "Welcome!"
+
+@app.route("/getCoffeeName/<spec>")
+def get_category(spec):
+    return str(test_model(spec))
+
+if __name__ == "__main__":
+    app.run()
